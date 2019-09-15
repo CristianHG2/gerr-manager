8bbd6e5f977c7d723f8f609f285552a499038ac0
Cristian-LS <cristian@lifespikes.com>
2019-09-15 19:28:11 +0000

    passhtru impl

--- install.php
+++ install.php
@@ -8,15 +8,15 @@ function install()
         IO::log('PHP OK');
     
         if (IO::confirm('Update PHP to PHP 7.3?')) {
-            exec('sudo add-apt-repository ppa:ondrej/php');
-            exec('sudo apt-get update');
-            exec('sudo apt-get install php7.3 php7.3-fpm phpcli phpmysql phpgd phpimagick phprecode phptidy phpxmlrpc');
+            passthru('sudo add-apt-repository ppa:ondrej/php');
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install php7.3 php7.3-fpm phpcli phpmysql phpgd phpimagick phprecode phptidy phpxmlrpc');
         }
     } else {
         if (IO::confirm('PHP 7.3 (FPM) NOT installed, install?')) {
-            exec('sudo add-apt-repository ppa:ondrej/php');
-            exec('sudo apt-get update');
-            exec('sudo apt-get install php7.3 php7.3-fpm phpcli phpmysql phpgd phpimagick phprecode phptidy phpxmlrpc');
+            passthru('sudo add-apt-repository ppa:ondrej/php');
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install php7.3 php7.3-fpm phpcli phpmysql phpgd phpimagick phprecode phptidy phpxmlrpc');
         } else {
             IO::abort();
         }
@@ -26,15 +26,15 @@ function install()
         IO::log('NGINX OK');
     
         if (IO::confirm('Update nginx?')) {
-            exec('sudo add-apt-repository ppa:nginx/stable');
-            exec('sudo apt-get update');
-            exec('sudo apt-get install nginx');
+            passthru('sudo add-apt-repository ppa:nginx/stable');
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install nginx');
         }
     } else {
         if (IO::confirm('Nginx NOT installed, install?')) {
-            exec('sudo add-apt-repository ppa:nginx/stable');
-            exec('sudo apt-get update');
-            exec('sudo apt-get install nginx');
+            passthru('sudo add-apt-repository ppa:nginx/stable');
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install nginx');
         } else {
             IO::abort();
         }
@@ -44,15 +44,15 @@ function install()
         IO::log('Composer OK');
 
         if (IO::confirm('Update composer?')) {
-            exec('composer self-update');
+            passthru('composer self-update');
         }
     } else {
         if (IO::confirm('Composer NOT installed, install?')) {
             $installer = file_get_contents('https://getcomposer.org/installer');
 	    file_put_contents('composer-install.php', $installer);
 
-	    exec('php composer-install.php');
-	    exec('sudo mv composer.phar /usr/local/bin/composer');
+	    passthru('php composer-install.php');
+	    passthru('sudo mv composer.phar /usr/local/bin/composer');
 
 	    IO::log('Composer installed');
         } else {
@@ -65,10 +65,10 @@ function install()
         IO::log('GERR doesn not support updating MySQL. Please update manually if applicable');
     } else {
         if (IO::confirm('MySQL/MariaDB NOT installed, install?')) {
-            exec('sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8');
-            exec('sudo add-apt-repository \'deb [arch=amd64,arm64,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.4/ubuntu bionic main\'');
-            exec('sudo apt-get update');
-            exec('sudo apt-get install mariadb-server');
+            passthru('sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xF1656F24C74CD1D8');
+            passthru('sudo add-apt-repository \'deb [arch=amd64,arm64,ppc64el] http://mirrors.coreix.net/mariadb/repo/10.4/ubuntu bionic main\'');
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install mariadb-server');
             IO::log('Please enter the username in config.json');
         } else {
             IO::abort();
@@ -77,19 +77,19 @@ function install()
 
     if (!file_exists('/usr/bin/gerr')) {
         if (IO::confirm('Make "gerr" executable globally available?')) {
-            exec('sudo ln -s '.__DIR__.'/gerr.php /usr/bin/gerr');
+            passthru('sudo ln -s '.__DIR__.'/gerr.php /usr/bin/gerr');
         }
     }
 
     if (`which certbot`) {
-	IO::log('Certbot OK');
+	    IO::log('Certbot OK');
 
-	if (IO::confirm('Update Certbot?')) {
-	    passthru('sudo apt-get update');
-	    passthru('sudo apt-get install --only-upgrade certbot');
-	}
+        if (IO::confirm('Update Certbot?')) {
+            passthru('sudo apt-get update');
+            passthru('sudo apt-get install --only-upgrade certbot');
+        }
     } else {
-	passthru('bash '.__DIR__.'/bash/certbot.sh');
-	IO::log('Certbot installed');
+        passthru('bash '.__DIR__.'/bash/certbot.sh');
+        IO::log('Certbot installed');
     }
 }
